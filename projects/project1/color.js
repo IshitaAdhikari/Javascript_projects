604da34ccf30159f7922bffc0abd7535326238c5
Ishita Adhikari <143061611+IshitaAdhikari@users.noreply.github.com>
2024-08-24 10:41:39 +0000

    changes

--- projects/project1/color.js
+++ projects/project1/color.js
@@ -15,8 +15,11 @@ buttons.forEach(function(button){
     if(e.target.id === 'blue'){
       body.style.backgroundColor = e.target.id
     }
-    if(e.target.id === 'white'){
+    if(e.target.id === 'yellow'){
       body.style.backgroundColor = e.target.id
     }
+    if(e.target.id === 'green'){
+        body.style.backgroundColor = e.target.id
+      }
   })
 });
